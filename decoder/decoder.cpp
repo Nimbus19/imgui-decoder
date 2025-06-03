62017fb042e0675cdaec4b0f018adfd4f96a07ea
Wythe <wythe.wang@wanin.tw>
2025-06-03 11:26:29 +0800

    feat: create and show GLtexture

--- decoder/decoder.cpp
+++ decoder/decoder.cpp
@@ -1,6 +1,7 @@
 #include "decoder.hpp"
 #include "imgui.h"
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
@@ -16,16 +17,38 @@ Decoder::Decoder(android_app* app)
 //------------------------------------------------------------------------------
 Decoder::~Decoder()
 {
-
+    DestroyTexture(textureID);
 }
 //------------------------------------------------------------------------------
 void Decoder::DrawUI()
 {
-    ImGui::Begin("Decoder");   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+    ImGui::SetNextWindowSize(ImVec2(1280, 640), ImGuiCond_Once);
+    ImGui::Begin("Decoder");
     ImGui::Text("Decode video");
-    if (ImGui::Button("Decode"))
+    if (ImGui::Button("ReadFile"))
     {
         ReadFile();
+    }
+    if (ImGui::Button("CreateTexture"))
+    {
+
+        uint8_t* data = (uint8_t*)malloc(width * height * 4);
+        for (int i = 0; i < width * height; ++i)
+        {
+            data[i * 4 + 0] = 255; // R
+            data[i * 4 + 1] = 255;   // G
+            data[i * 4 + 2] = 0;   // B
+            data[i * 4 + 3] = 255; // A
+        }
+        textureID = CreateTexture(width, height, data);
+        free(data);
+    }
+    if (textureID != 0)
+    {
+        ImGui::Text("OpenGL Texture:");
+        ImGui::Text("pointer = %x", textureID);
+        ImGui::Text("size = %d x %d", width, height);
+        ImGui::Image((ImTextureID)(intptr_t)textureID, ImVec2(width, height));
     }
     ImGui::End();
 }
@@ -36,21 +59,51 @@ bool Decoder::ReadFile()
     FILE* file = fopen(filePath, "r");
     if (file == nullptr)
     {
-        log("Failed to open file");
+        Log("Failed to open file");
         return false;
     }
 
     char buffer[256];
     while (fgets(buffer, sizeof(buffer), file))
     {
-        log("%s", buffer);
+        Log("%s", buffer);
     }
 
     fclose(file);
     return true;
 }
 //------------------------------------------------------------------------------
-void Decoder::log(const char *fmt, ...)
+GLuint Decoder::CreateTexture(int width, int height, const void* data)
+{
+    // create texture
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // set texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    // send data to GPU memory
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFlush();
+
+    return textureID;
+}
+//------------------------------------------------------------------------------
+void Decoder::DestroyTexture(GLuint textureID)
+{
+    if (textureID != 0)
+    {
+        glDeleteTextures(1, &textureID);
+        textureID = 0;
+    }
+}
+//------------------------------------------------------------------------------
+void Decoder::Log(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
--- decoder/decoder.hpp
+++ decoder/decoder.hpp
@@ -1,16 +1,26 @@
 #pragma once
 
+#   include <EGL/egl.h>
+#   include <GLES3/gl3.h>
+
 class Decoder
 {
 public:
     Decoder(struct android_app* app);
     virtual ~Decoder();
 
+    const int width = 64;
+    const int height = 64;
+
     void DrawUI();
     bool ReadFile();
 
+    static GLuint CreateTexture(int width, int height, const void* data = nullptr);
+    static void DestroyTexture(GLuint textureID);
+
 private:
     struct android_app* m_App;
+    GLuint textureID = 0;
 
-    static void log(const char* fmt, ...);
+    static void Log(const char* fmt, ...);
 };
